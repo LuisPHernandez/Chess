f0e0e69138c5cf80d4e7e2a6012741d24a88845e
LuisPHernandez <lphm0105@gmail.com>
2025-02-15 10:36:28 -0600

    Load and draw images in GUI

--- src/chessGUI.py
+++ src/chessGUI.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from board import *
 
 # Define global variables
@@ -8,13 +9,38 @@ SQUARE_SIZE = (SCREEN_WIDTH / 8)
 WHITE = (238, 238, 210)
 BLACK = (118, 150, 86)
 
+PIECE_IMAGES = {
+    "P": "white-pawn.png",
+    "p": "black-pawn.png",
+    "R": "white-rook.png",
+    "r": "black-rook.png",
+    "N": "white-knight.png",
+    "n": "black-knight.png",
+    "B": "white-bishop.png",
+    "b": "black-bishop.png",
+    "Q": "white-queen.png",
+    "q": "black-queen.png",
+    "K": "white-king.png",
+    "k": "black-king.png"
+}
+
 class ChessGUI:
     # Initializes the graphical interface
     def __init__(self):
         pygame.init()
         self.board = Board()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.load_images()
         self.running = True
+
+    # Loads images for chess pieces
+    def load_images(self):
+        self.images = {}
+        images_dir = os.path.join("static", "images") 
+
+        for piece, file in PIECE_IMAGES.items():
+            path = os.path.join(images_dir, file)
+            self.images[piece] = pygame.transform.scale(pygame.image.load(path), (SQUARE_SIZE, SQUARE_SIZE))
 
     # Draws board on the screen
     def draw_board(self):
@@ -24,9 +50,23 @@ class ChessGUI:
                 color = WHITE if (rank + file) % 2 == 0 else BLACK
                 pygame.draw.rect(self.screen, color, square)
 
+    # Draws pieces onto board
+    def draw_pieces(self):
+        fen = self.board.convert_to_FEN().split("\n")
+        for rank, line in enumerate(fen):
+            file = 0
+            for char in line:
+                if char.isdigit():
+                    file += int(char)
+                elif char in PIECE_IMAGES:
+                    self.screen.blit(self.images[char], (file * SQUARE_SIZE, rank * SQUARE_SIZE))
+                    file += 1
+
+    # Runs the main game loop
     def run(self):
         while self.running:
             self.draw_board()
+            self.draw_pieces()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
